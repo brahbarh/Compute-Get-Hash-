b7db922b632717fe9526fd5469e108540696cb00
brahbarh <33864932+brahbarh@users.noreply.github.com>
2017-11-21 12:58:46 +0100

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+[assembly: AssemblyCompany("www.subisoft.net")]
+[assembly: AssemblyCopyright("SubiSoft © 2015")]
+[assembly: AssemblyDescription("Compute MD5, SHA1, SHA256, SHA384, SHA512 file hash.")]
+[assembly: AssemblyFileVersion("4.4.0.0")]
+[assembly: AssemblyProduct("ComputeHash")]
+[assembly: AssemblyTitle("Compute Hash")]
+[assembly: AssemblyTrademark("SubiSoft")]
+[assembly: AssemblyVersion("4.4.0.0")]
+[assembly: CompilationRelaxations(8)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: Guid("4091a929-35fb-4abd-bfd8-f287eeb982bb")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows=true)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification=true)]
